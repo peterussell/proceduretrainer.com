79f61e6b448495334cd8730555d1a6311d9168fd
Pete Russell <pinkbatz@gmail.com>
2022-07-21 17:08:47 +1200

    Cleaned up CRA and folder structure. Added react-router.

---
+++ pt-web/src/pages/landing/index.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+  return (
+    <>
+      <h1>Welcome to ProcedureTrainer.com</h1>
+      <h3>Aircraft checklist and procedure trainer</h3>
+    </>
+  )
+}
+
+export default LandingPage;
